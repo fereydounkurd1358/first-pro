ef6ac31623693b519607b227d7c68829a217e158
fereydou <fereydoun@gmail.com>
2023-02-07 19:06:20 +0330

    convert Button is Active Now

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let input = document.getElementById('input');
 let convert = document.getElementById('convert');
+let change = document.getElementById('change');
 let result = document.getElementById('result');
+
+let heading = document.querySelector('.heading');
+let inputState = document.querySelector('.input-group-text span');
 let ourValue = document.querySelector('.card-title span');
 let convertedValue = document.querySelector('.card-text');
 
@@ -18,5 +22,24 @@ convert.addEventListener('click', ()=> {
     }
     
 
+})
+
+change.addEventListener('click', ()=> {
+
+    if (convertingState === 1) {
+        convertingState = 2
+    }else {
+        convertingState = 1
+    }
+
+    if (convertingState == 1) {
+        console.log('c to f');
+        heading.innerHTML = "Convert Celsius to Fahrenheit";
+        inputState.innerHTML = "C";
+    }else {
+        console.log('f to c');
+        heading.innerHTML = "Convert Fahrenheit to Celsius";
+        inputState.innerHTML = "F";
+    }
 
 })
