1e09c6953b21c2cc1c775621db6599898ef8f2d0
fereydou <fereydoun@gmail.com>
2023-02-07 18:48:24 +0330

    Convert Button Is Active Now

---
+++ script.js
@@ -0,0 +1,22 @@
+let input = document.getElementById('input');
+let convert = document.getElementById('convert');
+let result = document.getElementById('result');
+let ourValue = document.querySelector('.card-title span');
+let convertedValue = document.querySelector('.card-text');
+
+let convertingState = 1 // 1 return C => F && 2 return F to C
+
+convert.addEventListener('click', ()=> {
+
+    let inputValue = parseInt(input.value);
+
+    if (convertingState === 1) {
+        result.style.display = 'block';
+        ourValue.innerHTML = input.value;
+        convertedValue.innerHTML = "= "+((inputValue*9/5)+32)+" <sup>o</sup>F";
+        input.value = ''
+    }
+    
+
+
+})
